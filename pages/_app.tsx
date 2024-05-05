d72d1d2c5d6da4ced9da04ce784ac1b370f523a7
mushahidjaved <javedmushahid@gmail.com>
2024-05-05 06:59:09 +0530

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,6 @@
+import App, { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+export default MyApp;
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React, { useEffect, useState, useRef } from "react";
+
+const Index = () => {
+  return (
+    <Box ml={2} mr={2}>
+      Weekday
+    </Box>
+  );
+};
+
+export default Index;
